69f610b678c84afece6b5492f939bd9bf40d61e3
juan pablo <jisaza@tappsi.co>
2017-05-08 21:26:07 -0500

    removes print

--- testing_webpage/business/views.py
+++ testing_webpage/business/views.py
@@ -5,5 +5,4 @@ def index(request):
     """
     will render and have the same view as /beta_invite except for message customization.
     """
-    print('on business index method')
     return beta_views.inner_index(request, is_user_site=False)
