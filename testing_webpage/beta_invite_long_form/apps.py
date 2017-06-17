c8896907b923572e426b22a05a855162da3faa2e
Juan Pablo <juan.isaza@voiq.com>
2017-06-17 17:15:25 -0500

    testing lots of stuff

---
+++ testing_webpage/beta_invite_long_form/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BetaInviteLongFormConfig(AppConfig):
+    name = 'beta_invite_long_form'
---
+++ testing_webpage/beta_invite_long_form/views.py
@@ -0,0 +1,8 @@
+from beta_invite import views as beta_views
+
+
+def index(request):
+    """
+    will render and have the same view as /beta_invite except for message customization.
+    """
+    return beta_views.inner_index(request, is_user_site=True)
