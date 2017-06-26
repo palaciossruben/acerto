1904376c378803812a4cab936b827b2051fd1b65
Juan Pablo <juan.isaza@voiq.com>
2017-06-26 13:36:54 -0500

    improves email stuff

---
+++ testing_webpage/SQL/update_target_variables_of_first_profiles.sql
@@ -0,0 +1,5 @@
+UPDATE TABLE users
+SET age=25, experience=2, profession_id=, country_id=1, education_level_id=
+WHERE email='laura.caceres92@gmail.com';
+
+-- education_level=UNDERGRADUATE, age=25, experience=2, profession=DESIGN
--- testing_webpage/beta_invite/util/email_sender.py
+++ testing_webpage/beta_invite/util/email_sender.py
@@ -1,7 +1,9 @@
 import os
+import json
 import smtplib
 
 from email.mime.text import MIMEText
+from django.utils.translation import ugettext as _
 
 
 def get_current_path():
@@ -17,21 +19,34 @@ def get_first_name(complete_name):
     return complete_name.strip().split()[0]
 
 
-def send(user):
+def read_email_credentials():
+    """Returns a json with the credentials"""
+    json_data = open(os.path.join(get_current_path(), "email_credentials.json")).read()
+    return json.loads(json_data)
+
+
+def send(user, language_code):
 
     # Open a plain text file for reading.  For this example, assume that
     # the text file contains only ASCII characters.
-    with open(get_current_path() + '/email_body') as fp:
+
+    body_filename = 'email_body'
+    if language_code != 'en':
+        body_filename += '_{}'.format(language_code)
+
+    with open(os.path.join(get_current_path(), body_filename)) as fp:
         msg = MIMEText(fp.read().format(name=get_first_name(user.name)))
 
-    msg['Subject'] = 'Welcome to PeakU'
-    msg['From'] = 'biosolardecolombia@gmail.com'
+    email_data = read_email_credentials()
+
+    msg['Subject'] = _('Welcome to PeakU')
+    msg['From'] = email_data['email']
     msg['To'] = user.email
 
     # Send the message via our own SMTP server.
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
-    server.login("biosolardecolombia", "Aa112358")
+    server.login(email_data['email'], email_data['password'])
 
     server.send_message(msg)
     server.quit()
