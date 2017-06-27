627c19844acfc5295623203e22bff5982ab06e6e
Juan Pablo <juan.isaza@voiq.com>
2017-06-26 23:25:20 -0500

    integrates long form into beta_invite app

--- testing_webpage/beta_invite/constants.py
+++ testing_webpage/beta_invite/constants.py
@@ -1,3 +1,4 @@
 UI_VERSION = "1.1.1 - changes name placeholder to 'Your Complete Name'"
 BETA_INVITE_VIEW_PATH = 'beta_invite/index.html'
 SUCCESS_VIEW_PATH = 'beta_invite/success.html'
+LONG_FORM_VIEW_PATH = 'beta_invite/long_form.html'
