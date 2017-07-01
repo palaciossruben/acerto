7ce7abcae5a0e83d67839589626738ab437ee7e0
Juan Pablo <juan.isaza@voiq.com>
2017-07-01 13:45:50 -0500

    comments printing

--- testing_webpage/subscribe/search_engine.py
+++ testing_webpage/subscribe/search_engine.py
@@ -72,8 +72,8 @@ def save_dictionary_scores(path):
         scores.append((word_str, score))
 
     scores.sort(key=lambda x: x[1])
-    for word_str, score in scores:
-        print('{}: {}'.format(word_str, score))
+    #for word_str, score in scores:
+    #    print('{}: {}'.format(word_str, score))
 
     pickle.dump(scores, open('autocomplete_scores.p', 'wb'))
 
