3d6f245dc28367d6e881851808e3066ca45c3836
Juan Pablo <juan.isaza@voiq.com>
2017-07-17 15:59:11 -0500

    improves pricing table

---
+++ testing_webpage/business/static/js/price.js
@@ -0,0 +1,17 @@
+function ClickOnStandard(){
+    $('#standard_header').css("background", "green");
+    $('#agile_header').css("background", "black");
+    $('#custom_header').css("background", "black");
+}
+
+function ClickOnAgile(){
+    $('#standard_header').css("background", "black");
+    $('#agile_header').css("background", "green");
+    $('#custom_header').css("background", "black");
+}
+
+function ClickOnCustom(){
+    $('#standard_header').css("background", "black");
+    $('#agile_header').css("background", "black");
+    $('#custom_header').css("background", "green");
+}
