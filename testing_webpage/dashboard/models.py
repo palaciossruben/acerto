57736e1ef23190f834455185439efce3c63ebfa2
Juan Pablo <juan.isaza@voiq.com>
2017-09-14 16:34:35 -0500

    adds is_rejected field on the states model

--- testing_webpage/dashboard/models.py
+++ testing_webpage/dashboard/models.py
@@ -7,6 +7,7 @@ class State(models.Model):
 
     name = models.CharField(max_length=200)
     priority = models.IntegerField(default=1)
+    is_rejected = models.BooleanField(default=False)
 
     def __str__(self):
         return '{0}, {1}'.format(self.id, self.name)
