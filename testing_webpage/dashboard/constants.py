147bfb4d37542874520bd195a0892173a7f62ac8
Juan Pablo <juan.isaza@voiq.com>
2017-11-02 11:20:17 -0500

    adds video interviews. Editing on dashboard (first version)

--- testing_webpage/dashboard/constants.py
+++ testing_webpage/dashboard/constants.py
@@ -2,5 +2,6 @@ MAIN_DASHBOARD = 'dashboard/index.html'
 DASHBOARD_EDIT = 'dashboard/campaign/edit.html'
 NEW_TEST = 'dashboard/test/new.html'
 NEW_CAMPAIGN = 'dashboard/campaign/new.html'
+INTERVIEW = 'dashboard/campaign/interview.html'
 DEFAULT_STATE = 1
 CAMPAIGN_ID_NULL = 0
