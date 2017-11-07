32bc4893c6e4bd03945d53a07c1c0f061923eb06
Juan Pablo <juan.isaza@voiq.com>
2017-11-06 19:27:51 -0500

    adds ssh support

--- testing_webpage/fabfile.py
+++ testing_webpage/fabfile.py
@@ -152,5 +152,5 @@ def deploy():
             for f in fixtures_dirs:
                 run('python3 manage.py loaddata {}'.format(f))
 
-            # Last step: start gunicorn as a deamon.
-            run('PYENV_VERSION=3.5.2 gunicorn -c gunicorn_cfg.py testing_webpage.wsgi')
+            # Last step: start gunicorn as a deamon: that binds to a unix socket, from where nginx listens.
+            run('PYENV_VERSION=3.5.2 gunicorn -c gunicorn_cfg.py testing_webpage.wsgi --bind=unix:/opt/peaku_co/run/gunicorn.sock')
