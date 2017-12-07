b7c16d5f744bc70fb67f25b3bcd5ab378a3995ea
jprendon <ing.pendiente@gmail.com>
2017-12-06 19:25:39 -0500

    half baked functionality

--- testing_webpage/send_user_reminder_emails.py
+++ testing_webpage/send_user_reminder_emails.py
@@ -15,15 +15,14 @@ from beta_invite.util import email_sender
 from dashboard.models import Candidate
 
 
-def get_recently_created_backlog_candidates():
+def get_recently_created_new_state_candidates(state):
     """
-    Returns: Gets candidates which are on backlog, created in between 1 and 25 hours ago.
+    Returns: Gets candidates which are on a new state, created in between 1 and 25 hours ago.
     """
     # TODO: missing boolean indicating whether the email was sent.
     start_date = datetime.utcnow() - timedelta(hours=25)
     end_date = datetime.utcnow() - timedelta(hours=1)
-    return Candidate.objects.filter(state__name="Backlog", created_at__range=(start_date, end_date))
-
+    return Candidate.objects.filter(state__name=state, created_at__range=(start_date, end_date))
 
 def translate_email_subject(user):
     """
@@ -38,20 +37,22 @@ def translate_email_subject(user):
         return 'I invite you to do the test for {campaign}'.format(campaign=user.campaign.title)
 
 
-def send_test_reminder():
+def send_test_reminder(email_template, state):
     """
     Sends a reminder to all users who have registered in between 25 to 1 hour ago, asking to complete the tests.
     Returns: send email like crazy
     """
 
-    candidates = get_recently_created_backlog_candidates()
+    candidates = get_recently_created_new_state_candidates(state)
 
     for candidate in candidates:
         user = candidate.user
         email_sender.send(users=user,
                           language_code=user.language_code,
-                          body_input='user_test_reminder_email_body',
+                          body_input=email_template,
                           subject=translate_email_subject(user))
 
 
-send_test_reminder()
+send_test_reminder('user_test_reminder_email_body', 'Backlog')
+send_test_reminder('', 'Waiting for Interview')
+
