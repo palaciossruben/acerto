d3493c1b5df0a77102c3c1541130de1f251228f1
Juan Pablo <juan.isaza@voiq.com>
2017-12-13 18:07:45 -0500

    Starting with tabs for the Amazing Dashboard

---
+++ testing_webpage/business/static/js/business/tabs.js
@@ -0,0 +1,27 @@
+/* Runs when HTML loads */
+
+/*TODO: make it work*/
+/*var tab_links;
+tab_links = document.getElementsByClassName("tab-links");
+tab_links[0].currentTarget.className += " active";
+document.getElementById(step_name).style.display = "block";*/
+
+
+function tab(tab_active, step_name) {
+
+    var i, tab_content, tab_links;
+    tab_content = document.getElementsByClassName("tab-content");
+
+    for (i = 0; i < tab_content.length; i++) {
+        tab_content[i].style.display = "none";
+    }
+
+    tab_links = document.getElementsByClassName("tab-links");
+
+    for (i = 0; i < tab_links.length; i++) {
+        tab_links[i].className = tab_links[i].className.replace(" active", "");
+    }
+
+    document.getElementById(step_name).style.display = "block";
+    tab_active.currentTarget.className += " active";
+}
