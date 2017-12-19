dba05b51bcf4df65462718d7c9a7a3248166be05
Juan Pablo <juan.isaza@voiq.com>
2017-12-19 14:04:36 -0500

    adds business_user to login

--- testing_webpage/business/views.py
+++ testing_webpage/business/views.py
@@ -512,10 +512,10 @@ def home(request):
     Returns: displays all offers of a business
     """
 
-    #offers = Offer.objects.filter(business_user_id=get_business_user(request))
-    #return render(request, cts.HOME_VIEW_PATH, {'offers': offers,
-    #                                            })
-    return redirect('dashboard/79')
+    auth_user = request.user
+    business_user = BusinessUser.objects.get(auth_user=auth_user)
+
+    return redirect('dashboard/{}'.format(business_user.id))
 
 
 def send_contact_emails(contact, language_code):
