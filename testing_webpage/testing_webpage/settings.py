0832393d3805d9d006af7a6028b5e2c215415236
Juan Pablo Rendon <ing.pendiente@gmail.com>
2018-01-18 09:07:28 -0500

    sitemap views

--- testing_webpage/testing_webpage/settings.py
+++ testing_webpage/testing_webpage/settings.py
@@ -1,6 +1,1 @@
-from testing_webpage.shared_settings import *
-
-print('using development settings')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+settings.development.py
--- testing_webpage/testing_webpage/views.py
+++ testing_webpage/testing_webpage/views.py
@@ -1,4 +1,5 @@
 from beta_invite import views as beta_views
+from django.shortcuts import render
 
 
 def index(request):
@@ -6,3 +7,8 @@ def index(request):
     will render and have the same view as /beta_invite
     """
     return beta_views.index(request)
+
+
+def sitemap(request):
+
+    return render(request, 'testing_webpage/sitemap.xml', {}, content_type="application/xhtml+xml")
