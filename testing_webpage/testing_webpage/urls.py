15e814118c52fb9cdf3a7af2d34d198d29495726
Juan Pablo Rendon <ing.pendiente@gmail.com>
2018-01-29 17:48:10 -0500

    Send mail for default campaign bug fixed, the correct pwd_reset tamplates and url's location and remove duplicated question for one campaigns test

--- testing_webpage/testing_webpage/urls.py
+++ testing_webpage/testing_webpage/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls import include, url
 from django.contrib import admin
-
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -27,6 +27,12 @@ urlpatterns = [
     url(r'^business/', include('business.urls')),
     url(r'^dashboard/', include('dashboard.urls')),
     url(r'^admin/', admin.site.urls),
+
+    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 ]
 
 urlpatterns += i18n_patterns(
