c30c7de6cfa6365bfd9098112269cf95bdd3487b
Juan Pablo <juan.isaza@voiq.com>
2018-02-06 22:58:30 -0200

    adds migrations to see scores of each test, per candidate

--- testing_webpage/beta_invite/models.py
+++ testing_webpage/beta_invite/models.py
@@ -254,6 +254,22 @@ class Campaign(models.Model):
         return [b.name_es for b in self.bullets.all() if b.bullet_type.name == 'requirement']
 
 
+class Score(models.Model):
+
+    test = models.ForeignKey(Test)
+    value = models.FloatField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return 'id={0}, test={1}, value={2}'.format(self.pk, self.test, self.value)
+
+    # adds custom table name
+    class Meta:
+        db_table = 'scores'
+
+
 class Evaluation(models.Model):
     """
     Summary of all tests results for a given user.
@@ -263,6 +279,7 @@ class Evaluation(models.Model):
     cut_score = models.FloatField()
     final_score = models.FloatField()
     passed = models.BooleanField()
+    scores = models.ManyToManyField(Score)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -390,23 +407,6 @@ class TradeUser(models.Model):
     # adds custom table name
     class Meta:
         db_table = 'trade_users'
-
-
-class Score(models.Model):
-
-    user = models.ForeignKey(User, null=True)
-    test = models.ForeignKey(Test)
-    value = models.FloatField()
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return 'id={0}, user={1}, test={2}, value={3}'.format(self.pk, self.user, self.test, self.value)
-
-    # adds custom table name
-    class Meta:
-        db_table = 'scores'
 
 
 class PersonalityType(models.Model):
--- testing_webpage/beta_invite/test_module.py
+++ testing_webpage/beta_invite/test_module.py
@@ -129,8 +129,6 @@ def get_scores(campaign, user_id, questions_dict, request):
 
         score = Score(test_id=test_id,
                       value=test_score)
-        if user_id:
-            score.user_id = int(user_id)
         score.save()
 
         scores.append(test_score)
@@ -174,7 +172,8 @@ def get_evaluation(cut_scores, scores, campaign, user_id):
 
     evaluation = Evaluation(campaign=campaign,
                             cut_score=cut_score,
-                            final_score=final_score)
+                            final_score=final_score,
+                            scores=scores)
 
     evaluation.save()
 
