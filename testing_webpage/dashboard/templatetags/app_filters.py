2240a42cdaf37e8d89eeb442aff8ab2fbfebb25c
Juan Pablo Rendon <ing.pendiente@gmail.com>
2018-03-02 21:50:39 -0500

    Bug

--- testing_webpage/dashboard/templatetags/app_filters.py
+++ testing_webpage/dashboard/templatetags/app_filters.py
@@ -7,3 +7,15 @@ register = template.Library()
 @register.filter
 def get_campaign_url(campaign):
     return campaign.get_url()
+
+
+@register.filter
+def get_dashboard_campaign_url(campaign):
+        query = BusinessUser.objects.filter(campaigns__id__in=[campaign.pk])
+        user = [id for id in query][0]
+        return campaign.get_host()+'/seleccion_de_personal/tablero_de_control/{user_id}?campaign_id={campaign_id}'.format(user_id=user.id, campaign_id=campaign.pk)
+
+
+@register.filter
+def get_business_user_id_from_auth_user(user):
+    return BusinessUser.objects.get(auth_user=user).pk
