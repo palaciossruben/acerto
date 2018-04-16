68c4cceca8cf5768001aa2829443eb265867988a
Juan Pablo Rendon <ing.pendiente@gmail.com>
2018-04-16 15:29:12 -0500

    Some email and message bodys

--- testing_webpage/testing_webpage/send_candidate_emails.py
+++ testing_webpage/testing_webpage/send_candidate_emails.py
@@ -5,22 +5,23 @@ from django.db.models import Q
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testing_webpage.settings')
 application = get_wsgi_application()
 
-from beta_invite.util import messenger_sender
+from beta_invite.util import email_sender
 from dashboard.models import Candidate
 
 
-def send_candidates_messages():
+def send_candidates_emails():
 
-    candidates = Candidate.objects.filter(~Q(user=None), user__gender_id=None).order_by('-user_id')[:100]
+    candidates = Candidate.objects.filter(~Q(user=None), user__gender_id=None).order_by('-user_id')  # [:100]
 
     candidates = [c for c in candidates]
 
-    # print([c.user_id for c in candidates])
-    messenger_sender.send(candidates=candidates,
-                          language_code='es',
-                          body_input='candidates_form_reminder_message_body')
+    print([c.user_id for c in candidates])
+    email_sender.send(objects=candidates,
+                      language_code='es',
+                      body_input='candidates_form_reminder_email_body',
+                      subject='Información adicional')
 
 
 # Precaution: If script imported for another module, this lines avoid the execution of this entire file
 if __name__ == '__main__':
-    send_candidates_messages()
+    send_candidates_emails()
