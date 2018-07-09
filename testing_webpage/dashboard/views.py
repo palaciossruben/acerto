625c86ef0852eeb354f48f97694f64deab01f336
Juan Pablo Rendon <ing.pendiente@gmail.com>
2018-07-09 16:44:02 -0500

    small changes

--- testing_webpage/dashboard/views.py
+++ testing_webpage/dashboard/views.py
@@ -524,7 +524,7 @@ def mark_as_added(users):
 
 def send_new_contacts(request):
     """
-    Works as API for auto-messenger app
+     Works as API for auto-messenger app
     :param request: HTTP
     :return: json
     """
