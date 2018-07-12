f42a73e6c56c80a31d4bbaf709ffd7f31e96d8de
Juan Pablo Rendon <ing.pendiente@gmail.com>
2018-07-12 14:50:48 -0500

    Summary backend flow

--- testing_webpage/dashboard/views.py
+++ testing_webpage/dashboard/views.py
@@ -544,7 +544,8 @@ def send_new_contacts(request):
     """
 
     users = {m.candidate.user for m in Message.objects.filter(~Q(candidate__user__phone=None),
-                                                              sent=False)}  #[:50]
+                                                              sent=False)}  # [:50]
+
     for u in users:
         u.change_to_international_phone_number()
         u.name = email_sender.remove_accents(u.name)
