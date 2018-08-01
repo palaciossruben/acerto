d4b8b0d40887083efad6f95664b712deee743e8f
Juan Pablo <juan.isaza@voiq.com>
2018-08-01 01:04:41 -0400

    more learning

--- testing_webpage/learn_tests/main.py
+++ testing_webpage/learn_tests/main.py
@@ -12,9 +12,13 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testing_webpage.settings')
 application = get_wsgi_application()
 
 import pandas as pd
-from beta_invite.models import Test, Survey
+from beta_invite.models import Test, Survey, Question
 import common
 from dashboard.models import State
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+
 from match.pickle_models.pickle_handler import save_model, load_model
 from match import common_learning, learn, text_match, clustering
 from match.pickle_models import pickle_handler
@@ -51,14 +55,12 @@ question_ids = [q.id for q in cognitive_test.questions.all()]
 
 for survey in surveys:
 
-    if survey.score and survey.test.cut_score:
-        #print(survey.user)
-        #print(survey.campaign)
+    if survey.score is not None and survey.test.cut_score:
         candidate = common.get_candidate(survey.user, survey.campaign)
 
         if candidate:
-            print(survey.score)
-            print(survey.test.cut_score/100)
+            #print(survey.score)
+            #print(survey.test.cut_score/100)
             passed = 1 if survey.score > survey.test.cut_score/100 else 0
 
             if candidates.get(candidate) is None:
@@ -66,16 +68,53 @@ for survey in surveys:
 
             candidates[candidate][survey.question] = passed
 
-print(candidates)
+#print(candidates)
 
 df = pd.DataFrame(columns=question_ids + ['passed'], index=[c.id for c in candidates.keys()])
 
 
 for candidate, values in candidates.items():
-    passed_test = 1 if candidate.state in State.get_relevant_states() else 0
-    for question, passed in values.items():
-        df.loc[candidate.id, question.id] = passed
+
+    # df.loc[candidate.id, 'passed'] = int(sum(values.values()) > 4)
+
+    if candidate.state in State.get_rejected_states() + State.get_recommended_states():
+        df.loc[candidate.id, 'passed'] = 1 if candidate.state in State.get_recommended_states() else 0
+        #df.loc[candidate.id, 'passed'] = 1 if candidate.state in State.get_relevant_states() else 0
+
+        for question, passed in values.items():
+            df.loc[candidate.id, question.id] = passed
+
+df.dropna(axis=0, inplace=True)
 
 print(df)
 
 
+y = list(df['passed'])
+df.drop('passed', axis=1, inplace=True)
+
+trials = ['trial 1', 'trial 2', 'trial 3']
+importance = pd.DataFrame(columns=list(df))
+cross_val_scores = pd.DataFrame(columns=trials)
+for seed in range(20):
+    X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=0.33, random_state=seed)
+
+    model = RandomForestClassifier(max_depth=4, n_estimators=10)
+    model.fit(X_train, y_train)
+
+    c = [float(e) for e in cross_val_score(model, X_test, y_test, scoring='accuracy')]
+    cross_val_scores = cross_val_scores.append({col: value for col, value in zip(trials, c)}, ignore_index=True)
+    p = model.feature_importances_
+    importance = importance.append({col: value for col, value in zip(list(df), p)}, ignore_index=True)
+
+print(cross_val_scores)
+
+#tmp = list(cross_val_scores['trial 1'])
+#print(tmp)
+#import statistics
+#import numpy as np
+#print('avg accuracy: ' + np.mean(list(cross_val_scores['trial 1'])))
+
+print('importance avg: ' + str(importance.mean()))
+print('importance median: ' + str(importance.median()))
+print('importance std: ' + str(importance.std()))
+print([q for q in Question.objects.filter(pk__in=question_ids)])
