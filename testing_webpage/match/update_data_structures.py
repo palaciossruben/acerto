6128e6a9a845a50483d9f02eb4e4d4d1927a650b
Juan Pablo <juan.isaza@voiq.com>
2018-08-20 23:34:24 -0300

    adds model to stracture

--- testing_webpage/match/update_data_structures.py
+++ testing_webpage/match/update_data_structures.py
@@ -1,11 +1,11 @@
 import time
 from datetime import datetime
-#from match.model import save_other_values
-from model import save_other_values
+from model import save_other_values, run
 
 
 if __name__ == '__main__':
     t0 = time.time()
+    run()
     save_other_values()
     t1 = time.time()
     print('On {0} {1}, took: {2}'.format(datetime.today(), __file__, t1 - t0))
