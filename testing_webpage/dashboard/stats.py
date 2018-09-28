acbe045e3c669850dc40f400d8a21cd1e2f20420
Juan Pablo <juan.isaza@voiq.com>
2018-09-27 23:03:25 -0300

    adds forecast graphs

--- testing_webpage/dashboard/stats.py
+++ testing_webpage/dashboard/stats.py
@@ -6,9 +6,32 @@ import pandas as pd
 from django.shortcuts import render
 from fusioncharts import FusionCharts
 
-from dashboard.models import Candidate
+from dashboard.models import Candidate, StateEvent
 from dashboard import constants as cts
 from beta_invite.models import Campaign
+
+
+CHART = {
+    "xAxisName": "Month",
+    "yAxisName": "Count",
+    "numberPrefix": "",
+    "paletteColors": "#0075c2",
+    "bgColor": "#ffffff",
+    "borderAlpha": "0",
+    "canvasBorderAlpha": "0",
+    "usePlotGradientColor": "0",
+    "plotBorderAlpha": "10",
+    "placevaluesInside": "1",
+    "rotatevalues": "1",
+    "valueFontColor": "#ffffff",
+    "showXAxisLine": "1",
+    "xAxisLineColor": "#999999",
+    "divlineColor": "#999999",
+    "divLineIsDashed": "1",
+    "showAlternateHGridColor": "0",
+    "subcaptionFontBold": "0",
+    "subcaptionFontSize": "14"
+}
 
 
 def get_month_format(number):
@@ -21,29 +44,8 @@ def get_month_format(number):
 def candidates_count(request):
     # Chart data is passed to the `dataSource` parameter, as dict, in the form of key-value pairs.
     data_source = dict()
-    data_source['chart'] = {
-        "caption": "Total candidates registrations",
-        "subCaption": "per month",
-        "xAxisName": "Month",
-        "yAxisName": "Count",
-        "numberPrefix": "",
-        "paletteColors": "#0075c2",
-        "bgColor": "#ffffff",
-        "borderAlpha": "0",
-        "canvasBorderAlpha": "0",
-        "usePlotGradientColor": "0",
-        "plotBorderAlpha": "10",
-        "placevaluesInside": "1",
-        "rotatevalues": "1",
-        "valueFontColor": "#ffffff",
-        "showXAxisLine": "1",
-        "xAxisLineColor": "#999999",
-        "divlineColor": "#999999",
-        "divLineIsDashed": "1",
-        "showAlternateHGridColor": "0",
-        "subcaptionFontBold": "0",
-        "subcaptionFontSize": "14"
-    }
+    CHART["caption"] = "Total candidates registrations"
+    data_source['chart'] = CHART
 
     data_source['data'] = []
 
@@ -73,29 +75,8 @@ def candidates_count(request):
 def campaign_count(request):
     # Chart data is passed to the `dataSource` parameter, as dict, in the form of key-value pairs.
     data_source = dict()
-    data_source['chart'] = {
-        "caption": "Total campaigns registrations",
-        "subCaption": "per month",
-        "xAxisName": "Month",
-        "yAxisName": "Count",
-        "numberPrefix": "",
-        "paletteColors": "#0075c2",
-        "bgColor": "#ffffff",
-        "borderAlpha": "0",
-        "canvasBorderAlpha": "0",
-        "usePlotGradientColor": "0",
-        "plotBorderAlpha": "10",
-        "placevaluesInside": "1",
-        "rotatevalues": "1",
-        "valueFontColor": "#ffffff",
-        "showXAxisLine": "1",
-        "xAxisLineColor": "#999999",
-        "divlineColor": "#999999",
-        "divLineIsDashed": "1",
-        "showAlternateHGridColor": "0",
-        "subcaptionFontBold": "0",
-        "subcaptionFontSize": "14"
-    }
+    CHART["caption"] = "Total campaign registrations"
+    data_source['chart'] = CHART
 
     data_source['data'] = []
 
@@ -122,5 +103,56 @@ def campaign_count(request):
     return render(request, cts.STATS_INDEX, {'output': column_2d.render()})
 
 
+# TODO: add
 def operational_efficiency():
     pass
+
+
+def render_forecast(request, graph_type):
+
+    data_source = dict()
+    CHART["caption"] = "Total Forecasts"
+    data_source['chart'] = CHART
+
+    data_source['data'] = []
+
+    if graph_type == 'all':
+        events = [e for e in StateEvent.objects.filter(use_machine_learning=True)]
+    elif graph_type == 'positive':
+        events = [e for e in StateEvent.objects.filter(use_machine_learning=True, forecast=True)]
+    elif graph_type == 'negative':
+        events = [e for e in StateEvent.objects.filter(use_machine_learning=True, forecast=False)]
+    else:
+        raise NotImplementedError('abraço apertado')
+
+    data = pd.DataFrame()
+    data['id'] = [c.pk for c in events]
+    data['created_at'] = [c.created_at for c in events]
+    data['month'] = data['created_at'].apply(lambda date: '{y}-{m}'.format(y=date.year,
+                                                                           m=get_month_format(date.month)))
+    data.sort_values(by=['month'], inplace=True)
+
+    gp = pd.groupby(data, by='month').aggregate({'id': 'count'})
+    gp = pd.DataFrame(gp)
+
+    for idx, row in gp.iterrows():
+        data = dict()
+        data['label'] = idx
+        data['value'] = str(row['id'])
+        data_source['data'].append(data)
+
+    # Create an object for the Column 2D chart using the FusionCharts class constructor
+    column_2d = FusionCharts("column2D", "ex1", "600", "350", "chart-1", "json", data_source)
+    return render(request, cts.STATS_INDEX, {'output': column_2d.render()})
+
+
+def number_of_forecasts(request):
+    return render_forecast(request, 'all')
+
+
+def positive_forecasts(request):
+    return render_forecast(request, 'positive')
+
+
+def negative_forecasts(request):
+    return render_forecast(request, 'negative')
