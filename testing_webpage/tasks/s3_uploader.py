aacc62e3e8ead74be23c1c2413664f71a4bbaad2
Juan Pablo <juan.isaza@voiq.com>
2018-09-30 11:42:03 -0300

    s3 daemon

--- testing_webpage/tasks/s3_uploader.py
+++ testing_webpage/tasks/s3_uploader.py
@@ -17,19 +17,10 @@ import boto3
 import time
 import urllib.parse
 from botocore.exceptions import EndpointConnectionError
-from threading import Thread
 from beta_invite.models import User
 from decouple import config
 from django.db.models import Q
 from queue import Queue
-
-
-NUM_WORKERS = 1
-WAITING_TIME_WORKERS = 10  # seconds
-WAITING_TIME_DB = 60  # 10 minutes
-#users_queue = Queue()
-NUM_PROCESS = 1
-DEBUG = False
 
 
 # the actual upload
@@ -95,54 +86,51 @@ def get_local_path(user):
 
 
 # each worker does this job
-def upload_users(users_queue):
+def upload_users(users_queue, wait_time_workers, debug):
     """
     Uploads a User to s3, then waits some time, and repeats...
     :return:
     """
 
     user = users_queue.get()
-    if DEBUG:
+    if debug:
         user.curriculum_s3_url = 'LE FINI'
     else:
         user.curriculum_s3_url = upload_resource_to_s3(user)
     user.save()
-    time.sleep(WAITING_TIME_WORKERS)
+    time.sleep(wait_time_workers)
 
     return users_queue
 
 
 # each worker does this job
-def upload_users_while_loop(users_queue):
+def upload_users_while_loop(users_queue, wait_time_workers, debug):
     """
     Uploads a User to s3, then waits some time, and repeats...
     :return:
     """
     while True:
-        upload_users(users_queue)
-
-
-def init_workers(num_workers=NUM_WORKERS):
-    for _ in range(num_workers):
-        t = Thread(target=upload_users_while_loop)
-        t.daemon = True
-        t.start()
+        upload_users(users_queue, wait_time_workers, debug)
 
 
 def run():
+
+    wait_time_workers = 10  # seconds
+    wait_time_db = 60  # 10 minutes
+    num_process = 1
+    debug = False
     users_queue = Queue()
 
-    #init_workers()
     created_since = datetime(day=9, month=4, year=1948)
 
     while True:
         if users_queue.empty:
             users_queue, created_since = add_new_users(users_queue, created_since)
 
-        for _ in range(NUM_PROCESS):
-            users_queue = upload_users(users_queue)
+        for _ in range(num_process):
+            users_queue = upload_users(users_queue, wait_time_workers, debug)
 
-        time.sleep(WAITING_TIME_DB)
+        time.sleep(wait_time_db)
 
 
 if __name__ == '__main__':
