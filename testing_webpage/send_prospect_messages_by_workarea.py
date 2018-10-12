d4137e63a717251b9f99657fd15f3273155c044d
Juan Pablo Rendon <ing.pendiente@gmail.com>
2018-10-12 11:49:35 -0500

    Prospects wp ready

--- testing_webpage/send_prospect_messages_by_workarea.py
+++ testing_webpage/send_prospect_messages_by_workarea.py
@@ -16,11 +16,11 @@ def send_prospect_messages(segment_code):
     candidates = Candidate.objects.filter(~Q(user__phone=None), user__work_area__segment=WorkAreaSegment.objects.get(code=segment_code)).order_by('-user_id').all()[:100]
 
     print([c.user_id for c in candidates])
-    '''
+
     messenger_sender.send(candidates=candidates,
                           language_code='es',
                           body_input='prospects_invitation_message_body')
-    '''
+
 
 # Precaution: If script imported for another module, this lines avoid the execution of this entire file
 if __name__ == '__main__':
