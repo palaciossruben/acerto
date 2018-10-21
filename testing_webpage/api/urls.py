dff222142800d42303f1fed86c02fe25bbf190bd
Juan Pablo Rendon <ing.pendiente@gmail.com>
2018-10-20 20:45:37 -0500

    Fulgencio changes

--- testing_webpage/api/urls.py
+++ testing_webpage/api/urls.py
@@ -31,6 +31,7 @@ app_name = 'api'
 urlpatterns = [
     url(r'^get_public_posts$', views.get_public_posts, name='get_public_posts'),
     url(r'^add_messages$', views.add_messages, name='add_messages'),
+    url(r'^save_leads$', views.save_leads, name='save_leads'),
     #url(r'^', include(router.urls)),
     #url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
