671c4ac22891198e7da7edddcdaaf8b994e3da42
Juan Pablo <juan.isaza@voiq.com>
2018-10-22 00:57:38 -0300

    turn off 2 process in favor of moving to es

--- testing_webpage/process_manager.py
+++ testing_webpage/process_manager.py
@@ -52,7 +52,7 @@ if __name__ == '__main__':
 
     h.log('PROCESS MANAGER STARTED')
     run_watchdog(document_reader_run, limit_hours=2)
-    run_watchdog(search_engine_run, limit_hours=3)
-    run_watchdog(context_search_run, limit_hours=3)
+    #run_watchdog(search_engine_run, limit_hours=3)
+    #run_watchdog(context_search_run, limit_hours=3)
     run_watchdog(model_run, limit_hours=1)
     h.log('PROCESS MANAGER FINISHED')
