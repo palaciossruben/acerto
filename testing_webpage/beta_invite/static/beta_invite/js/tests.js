e99532e04db8c13d9586fe687681b8dd52d91e79
Juan Pablo Rendon <ing.pendiente@gmail.com>
2018-11-07 14:03:05 -0500

    Some tests UI fixes

--- testing_webpage/beta_invite/static/beta_invite/js/tests.js
+++ testing_webpage/beta_invite/static/beta_invite/js/tests.js
@@ -28,3 +28,25 @@ function next(test_number, question_number, number_of_questions, number_of_tests
     }
     deactivate(test_number, question_number);
 }
+
+function timer(){
+    var minutesLabel = document.getElementById("minutes");
+    var secondsLabel = document.getElementById("seconds");
+    var totalSeconds = 0;
+    setInterval(setTime, 1000);
+
+    function setTime() {
+        ++totalSeconds;
+        secondsLabel.innerHTML = pad(totalSeconds % 60);
+        minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+    }
+
+    function pad(val) {
+        var valString = val + "";
+        if (valString.length < 2) {
+            return "0" + valString;
+        } else {
+            return valString;
+        }
+    }
+}
