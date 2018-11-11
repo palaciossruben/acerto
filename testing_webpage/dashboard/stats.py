784b2581c9ea60271db4e87469c3e04796caca54
Juan Pablo <juan.isaza@voiq.com>
2018-11-11 16:07:47 -0300

    small changes

--- testing_webpage/dashboard/stats.py
+++ testing_webpage/dashboard/stats.py
@@ -2,11 +2,13 @@
 Has views of stats.
 """
 
+import datetime
 import pandas as pd
 from django.shortcuts import render
 from fusioncharts import FusionCharts
+from django.db.models import Q
 
-from dashboard.models import Candidate, StateEvent
+from dashboard.models import Candidate, StateEvent, State
 from dashboard import constants as cts
 from beta_invite.models import Campaign, User
 
@@ -137,8 +139,6 @@ def render_forecast(request, graph_type):
     for idx, row in gp.iterrows():
         data_source['data'].append({'label': idx, 'value': str(row['id'])})
 
-    print(data_source['data'])
-
     # Create an object for the Column 2D chart using the FusionCharts class constructor
     column_2d = FusionCharts("column2D", "ex1", "600", "350", "chart-1", "json", data_source)
     return render(request, cts.STATS_INDEX, {'output': column_2d.render()})
@@ -158,25 +158,36 @@ def negative_forecasts(request):
 
 # TODO: complete this shit!
 def candidates_per_new_user(request):
+    """
+    This is equivalent to this SQL:
+    select s.m, cast(s.candidate_count as float) / cast(s.user_count as float) from
+        (select date_trunc('month', u.created_at) as m,
+                count(distinct u.id) user_count,
+                count(distinct c.id) candidate_count
+                from candidates c
+                inner join users u on c.user_id = u.id
+                where not removed and c.state_id != 11
+                and u.created_at > '2018-01-01'
+                group by m order by m) as s;
+    :param request:
+    :return:
+    """
 
     data_source = dict()
     CHART["caption"] = "Average candidates per user"
     data_source['chart'] = CHART
 
     columns = ['id', 'user_id', 'user__created_at']
-    data = pd.DataFrame(list(Candidate.objects.all().values_list(*columns)), columns=columns)
+    data = pd.DataFrame(list(Candidate.objects.filter(~Q(state=State.objects.get(code='P')),
+                                                      removed=False,
+                                                      user__created_at__gt=datetime.datetime(year=2018, month=1, day=1)).values_list(*columns)), columns=columns)
     data['month'] = data['user__created_at'].apply(lambda date: '{y}-{m}'.format(y=date.year,
                                                                                  m=get_month_format(date.month)))
     data.drop('user__created_at', inplace=True, axis=1)
 
-    print(data)
-
     gp = pd.groupby(data, by=['month', 'user_id']).aggregate({'id': 'count'})
     data = pd.DataFrame(gp)
     data.reset_index(inplace=True)
-
-    print('after first agg:')
-    print(data)
 
     gp = pd.groupby(data, by=['month']).aggregate({'user_id': 'count',
                                                    'id': 'sum'})
@@ -188,10 +199,22 @@ def candidates_per_new_user(request):
 
     data_source['data'] = []
     for idx, row in data.iterrows():
-        data_source['data'].append({'label': idx, 'value': str(row['id'] / row['user_id'])})
-
-    print(data_source['data'])
+        data_source['data'].append({'label': idx, 'value': str(round(row['id'] / row['user_id'], 2))})
 
     # Create an object for the Column 2D chart using the FusionCharts class constructor
     column_2d = FusionCharts("column2D", "ex1", "600", "350", "chart-1", "json", data_source)
     return render(request, cts.STATS_INDEX, {'output': column_2d.render()})
+
+
+# TODO: implement, more than one campaign counter:
+"""
+select s.m,
+       s.candidate_count - s.user_count
+       from (select date_trunc('month', u.created_at) as m,
+                    count(distinct u.id) user_count,
+                    count(distinct c.id) candidate_count
+             from candidates c inner join users u on c.user_id = u.id
+                    where not removed and c.state_id != 11
+                    and u.created_at > '2018-01-01'
+                    group by m order by m) as s;
+"""
