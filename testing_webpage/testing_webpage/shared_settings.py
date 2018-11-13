c9e625e9762216147d7131b1c02bb6b0f35d5cbe
Juan Pablo Rendon <ing.pendiente@gmail.com>
2018-11-13 18:12:30 -0500

    Cache...

--- testing_webpage/testing_webpage/shared_settings.py
+++ testing_webpage/testing_webpage/shared_settings.py
@@ -226,8 +226,5 @@ REST_FRAMEWORK = {
 SOCIAL_AUTH_FACEBOOK_KEY = config('social_auth_facebook_key')  # App ID
 SOCIAL_AUTH_FACEBOOK_SECRET = config('social_auth_facebook_secret')  # App Secret
 
-
-#'django.contrib.staticfiles.storage.CachedStaticFilesStorage'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 # This is for clearing the browser cache
-#STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
