ea6927128adb7afd11769ef3bffdb5b55e7b3bbf
Juan Pablo <juan.isaza@voiq.com>
2018-11-15 17:45:17 -0300

    adds a machete

--- testing_webpage/testing_webpage/storage.py
+++ testing_webpage/testing_webpage/storage.py
@@ -3,3 +3,5 @@ from django.contrib.staticfiles.storage import ManifestStaticFilesStorage
 
 class MyManifestStaticFilesStorage(ManifestStaticFilesStorage):
     manifest_strict = False
+    ignore_patterns = ['test.js', 'start.js']  # your custom ignore list
+
