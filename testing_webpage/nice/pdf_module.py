ed31bc343b04e3b3b0d21e406df43a0cba985520
Juan Pablo Isaza <juan@peaku.co>
2018-12-09 23:52:09 -0300

    changes path of cvs more

--- testing_webpage/nice/pdf_module.py
+++ testing_webpage/nice/pdf_module.py
@@ -23,4 +23,4 @@ import pdfkit
 from testing_webpage import settings
 
 base_url = 'http://127.0.0.1:8000' if settings.DEBUG else 'https://peaku.co'
-pdfkit.from_url(base_url + '/cv', 'static/nice/cvs/cv.pdf')
+pdfkit.from_url(base_url + '/cv', './static/nice/cvs/cv.pdf')
