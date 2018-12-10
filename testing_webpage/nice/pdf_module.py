613f215de0d7393f51e1033b36fb53882dff8cde
Juan Pablo Isaza <juan@peaku.co>
2018-12-09 23:24:29 -0300

    adds some libs for command line

--- testing_webpage/nice/pdf_module.py
+++ testing_webpage/nice/pdf_module.py
@@ -1,3 +1,24 @@
+"""
+For this to work you have to install pdfkit and wkhtmltopdf see:
+https://github.com/JazzCore/python-pdfkit
+"""
+
+import os
+import sys
+import platform
+from django.core.wsgi import get_wsgi_application
+
+# Environment can use the models as if inside the Django app
+dir_separator = '\\' if 'Windows' == platform.system() else '/'
+# how deep is this file from the project working directory?
+dir_depth = len(''.join(os.getcwd().split('testing_webpage/', 1)[1]).split(dir_separator))
+path_to_add = dir_separator.join(os.getcwd().split(dir_separator)[:-dir_depth])
+sys.path.insert(0, path_to_add)
+
+# Environment can use the models as if inside the Django app
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testing_webpage.settings')
+application = get_wsgi_application()
+
 import pdfkit
 from testing_webpage import settings
 
