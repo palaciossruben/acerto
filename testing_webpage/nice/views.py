229229b5741701ec49fa272db4441400ad49e7dd
Juan Pablo Isaza <biosolardecolombia@gmail.com>
2018-12-15 16:21:50 -0300

    adds a better way of doing the cvs

--- testing_webpage/nice/views.py
+++ testing_webpage/nice/views.py
@@ -17,6 +17,8 @@ import os
 import pdfkit
 import urllib.parse
 from testing_webpage import settings
+from django.template.loader import get_template
+from django.template import Context
 from django.shortcuts import render
 from django.http import HttpResponse
 from dashboard.models import Candidate
@@ -24,6 +26,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 from nice.cts import *
 from nice import pdf_module
+
 
 def cv_test(request, candidate_id):
     candidate = Candidate.objects.get(pk=candidate_id)
@@ -34,7 +37,9 @@ def cv_test(request, candidate_id):
 def download_cv(request, candidate_id):
     #base_url = 'http://127.0.0.1:8000' if settings.DEBUG else 'https://peaku.co'
     filename = 'cv_{}.pdf'.format(candidate_id)
-    file_path = os.path.join('./nice', 'cv', filename)
+    #file_path = os.path.join('./nice', 'cv', filename)
+    file_path = 'cv.pdf'
+
     #content_url = urllib.parse.urljoin(base_url, 'seleccion-de-personal/perfil-del-candidato/{}'.format(candidate_id))
     #content_url = urllib.parse.urljoin(base_url, 'cv/{}'.format(candidate_id))
     #print(content_url)
@@ -42,12 +47,27 @@ def download_cv(request, candidate_id):
 
     #pdfkit.from_url(content_url, file_path)
     #return HttpResponse(200)
-    pdf_module.render_cv(candidate_id, path_to_cv='nice/cv')
+    #pdf_module.render_cv(candidate_id, path_to_cv='nice/cv')
+
+    candidate = Candidate.objects.get(pk=candidate_id)
+
+    print(os.getcwd())
+
+    #template = get_template(os.path.join('.', 'nice', 'templates', 'nice', 'elon_cv.html'))
+
+    #template_path = '/Users/juanpabloisaza/Desktop/masteringmymind/acerto/API/testing_webpage/nice/templates/nice/elon_cv.html'
+    template_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'templates/nice/elon_cv.html')
+    print(template_path)
+    template = get_template(template_path)
+    html = template.render({'candidate': candidate})  # Renders the template with the context data.
+    pdfkit.from_string(html, file_path)
 
     try:
         with open(file_path, 'rb') as f:
             response = HttpResponse(f, content_type='application/pdf')
             response['Content-Disposition'] = 'attachment; filename={}'.format(filename)
-            return response
+
+        os.remove(file_path)
+        return response
     except FileNotFoundError:
         return HttpResponse('Cannot find CV', status=404)
