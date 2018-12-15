ea788f69ec39aee30b1148c8dc119eb06bf7fbae
Juan Pablo Isaza <biosolardecolombia@gmail.com>
2018-12-15 17:04:35 -0300

    more css

--- testing_webpage/nice/views.py
+++ testing_webpage/nice/views.py
@@ -41,7 +41,7 @@ def download_cv(request, candidate_id):
     template = get_template(template_path)
     html = template.render({'candidate': candidate})  # Renders the template with the context data.
 
-    #css = os.path.join(nice_dir, 'static', 'nice', 'css', 'cv.css')
+    css = os.path.join(nice_dir, 'static', 'nice', 'css', 'cv.css')
     pdfkit.from_string(html, tmp_pdf, css=css)
     #pdfkit.from_string(html, tmp_pdf)
 
