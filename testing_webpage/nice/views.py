a8044d4c19fed2b35dea60ae392a968407a603de
Juan Pablo Isaza <biosolardecolombia@gmail.com>
2018-12-15 16:44:03 -0300

    and we have this stuff working

--- testing_webpage/nice/views.py
+++ testing_webpage/nice/views.py
@@ -15,17 +15,13 @@ https://unix.stackexchange.com/questions/192642/wkhtmltopdf-qxcbconnection-could
 """
 import os
 import pdfkit
-import urllib.parse
-from testing_webpage import settings
 from django.template.loader import get_template
-from django.template import Context
 from django.shortcuts import render
 from django.http import HttpResponse
 from dashboard.models import Candidate
 from django.views.decorators.csrf import csrf_exempt
-
+from testing_webpage import settings
 from nice.cts import *
-from nice import pdf_module
 
 
 def cv_test(request, candidate_id):
@@ -35,39 +31,26 @@ def cv_test(request, candidate_id):
 
 @csrf_exempt
 def download_cv(request, candidate_id):
-    #base_url = 'http://127.0.0.1:8000' if settings.DEBUG else 'https://peaku.co'
-    filename = 'cv_{}.pdf'.format(candidate_id)
-    #file_path = os.path.join('./nice', 'cv', filename)
-    file_path = 'cv.pdf'
-
-    #content_url = urllib.parse.urljoin(base_url, 'seleccion-de-personal/perfil-del-candidato/{}'.format(candidate_id))
-    #content_url = urllib.parse.urljoin(base_url, 'cv/{}'.format(candidate_id))
-    #print(content_url)
-    #print(file_path)
-
-    #pdfkit.from_url(content_url, file_path)
-    #return HttpResponse(200)
-    #pdf_module.render_cv(candidate_id, path_to_cv='nice/cv')
 
     candidate = Candidate.objects.get(pk=candidate_id)
+    filename = '{}_CV.pdf'.format(candidate.user.name)
+    tmp_pdf = 'cv_tmp.pdf'
+    nice_dir = os.path.dirname(os.path.realpath(__file__))
 
-    print(os.getcwd())
-
-    #template = get_template(os.path.join('.', 'nice', 'templates', 'nice', 'elon_cv.html'))
-
-    #template_path = '/Users/juanpabloisaza/Desktop/masteringmymind/acerto/API/testing_webpage/nice/templates/nice/elon_cv.html'
-    template_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'templates/nice/elon_cv.html')
-    print(template_path)
+    template_path = os.path.join(nice_dir, 'templates', 'nice', 'elon_cv.html')
     template = get_template(template_path)
     html = template.render({'candidate': candidate})  # Renders the template with the context data.
-    pdfkit.from_string(html, file_path)
+
+    print(html)
+    css = os.path.join(nice_dir, 'static', 'nice', 'css', 'cv.css')
+    pdfkit.from_string(html, tmp_pdf, css=css)
 
     try:
-        with open(file_path, 'rb') as f:
+        with open(tmp_pdf, 'rb') as f:
             response = HttpResponse(f, content_type='application/pdf')
             response['Content-Disposition'] = 'attachment; filename={}'.format(filename)
 
-        os.remove(file_path)
+        os.remove(tmp_pdf)
         return response
     except FileNotFoundError:
         return HttpResponse('Cannot find CV', status=404)
