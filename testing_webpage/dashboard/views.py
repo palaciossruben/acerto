4643d3ded3e99198842a67d9bbc66a34fcd24b66
Juan Pablo Isaza <biosolardecolombia@gmail.com>
2019-01-17 00:40:35 +0100

    removes time stuff

--- testing_webpage/dashboard/views.py
+++ testing_webpage/dashboard/views.py
@@ -648,8 +648,7 @@ def add_backlog_messages(message_filename):
 
     # TODO: add English
     candidates = [c for c in
-                  Candidate.objects.filter(Q(created_at__lt=datetime.datetime.today() - datetime.timedelta(hours=2)) &
-                                           Q(created_at__gt=datetime.datetime.today() - datetime.timedelta(days=5)) &
+                  Candidate.objects.filter(Q(created_at__gt=datetime.datetime.today() - datetime.timedelta(days=5)) &
                                            Q(state__code='BL') &
                                            Q(campaign__state__name='Active') &
                                            ~Q(message__filename=message_filename) &
