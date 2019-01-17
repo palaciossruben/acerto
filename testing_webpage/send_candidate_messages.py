c1b3f7154e27fdf4d4b8de2b961447fc1e81620c
Juan Pablo Isaza <biosolardecolombia@gmail.com>
2019-01-17 21:49:33 +0100

    small change

--- testing_webpage/send_candidate_messages.py
+++ testing_webpage/send_candidate_messages.py
@@ -27,29 +27,13 @@ def send_candidates_messages():
     #                                      ~Q(user__phone=None),
     #                                      user__gender_id=None).order_by('-user_id')  # [:100]
 
-    message = """
-    Hola {name}
-    
-    Tengo tu información por nuestra base de datos y te cuento que estamos buscando desarrolladores front end bilingues para Bogotá (salarios entre los 4-6 M) con conocimiento en Javascript. Si te interesa aplica acá:
-        
-    https://peaku.co/servicio-de-empleo/?campaign_id=561
-    
-    Si conoces a alguien que le interese estamos entregando un bono de 300k en Ktronix si el candidato es contratado. Solo tienes que pasarnos su contacto de whatsapp. 
-    
-    Gracias y quedo atento!
-    
-    Saludos
-    Juan Pablo de PeakU
-    """
-
     candidates = list(Candidate.objects.filter(user__pk=1929))
 
     print(len(candidates))
 
     messenger.send(objects=candidates,
-                   language_code='es',
-                   body_input=message,
-                   body_is_filename=False)
+                   language_code='en',
+                   body_input='comodin')
 
 
 if __name__ == '__main__':
